f847ba75e433edc8ce79df70738f73533447c1b5
Anthony Kremor <anthony@kremor.com.au>
2017-05-12 15:05:36 +1000

    Demo 'simple' heuristic function. Also make minor code changes.

--- WarehouseGrapher.jl
+++ WarehouseGrapher.jl
@@ -4,7 +4,12 @@ using Colors
 using Compose
 
 
-# TODO Perhaps a switch statement
+costs = rand(1:1000, 16*16*16*16, 1)
+function heuristic(v)
+    return costs[v];
+end
+
+
 function nodeValue(char)
     if char == "O"
         return 2;
@@ -15,11 +20,6 @@ function nodeValue(char)
     end
 end
 
-#function edgeCost(map, start_x, start_y, end_x, end_y)
-    # TODO Why is there a max() here?
-    # return nodeValue(map[start_x,start_y],map[start_x,start_y])
-#end
-
 
 function isValidMove(element)
     if((element=="I")||(element=="O")||(element=="H"))
@@ -29,34 +29,28 @@ function isValidMove(element)
     end
 end
 
+
 function checkNeighbour(map, adj_mat, dist_mat, i, j)
 
     # Offsets relative to the cell of interest
-    for x_offset in [-1, 0, 1]
-        for y_offset in [-1, 0, 1]
-            # Check it isn't entering node [i,j] itself
-            if((x_offset!=0)||(y_offset!=0))
-                # Set the coordinate of the neighbour we are examining
-                x = i + x_offset
-                y = j + y_offset
-                if ((x > 0) && (x <= size(map)[1]) && (y > 0) && (y <= size(map)[2]))
-                    if isValidMove(map[x,y])
-                        println("\tTRUE ",x," ",y, " ", map[x,y])
-                        adj_mat[(i-1)*size(map)[1] + j, (x-1)*size(map)[1] + y] = true
-                        dist_mat[(i-1)*size(map)[1] + j, (x-1)*size(map)[1] + y] = nodeValue(map[x,y])
+    for x_offset in [-1, 0, 1], y_offset in [-1, 0, 1]
+        # Check it isn't entering node [i,j] itself
+        if (!(x_offset == 0 && y_offset == 0))
+            # Set the coordinate of the neighbour we are examining
+            x = i + x_offset;
+            y = j + y_offset;
+            if ((x > 0) && (x <= size(map)[1]) && (y > 0) && (y <= size(map)[2]))
+                if isValidMove(map[x,y])
+                    println("\tTRUE ",x," ",y, " ", map[x,y])
+                    adj_mat[(i-1)*size(map)[1] + j, (x-1)*size(map)[1] + y] = true
+                    dist_mat[(i-1)*size(map)[1] + j, (x-1)*size(map)[1] + y] = nodeValue(map[x,y])
 
-                        println("\t\tTRUE ",(i-1)*size(map)[1] + j," ", (x-1)*size(map)[1] + (y));
-                    else
-                        #adj_mat[(i-1)*size(map)[1] + j, (x-1)*size(map)[1] + y] = false
-                        #dist_mat[(i-1)*size(map)[1] + j, (x-1)*size(map)[1] + y] = 0
-                        #println("\tFALSE ",x," ",y, " ", map[x,y])
-                    end
+                    println("\t\tTRUE ",(i-1)*size(map)[1] + j," ", (x-1)*size(map)[1] + (y));
                 end
             end
         end
     end
 end
-           
 
 
 function readMapCSV(filename, MAP_SIZE_X, MAP_SIZE_Y)
@@ -79,22 +73,15 @@ end
 
 
 function generateMapGraph(map)
-    
 
-    #adj_mat = Array(Bool,size(map)[1]*size(map)[2],size(map)[1]*size(map)[2])
+    # Set up adjacency and distance matrices, prefill with 'empty'
     adj_mat = fill(false, size(map)[1]*size(map)[2],size(map)[1]*size(map)[2]);
-
-    #print(adj_mat);
-    #dist_mat = Array(Int64,size(map)[1]*size(map)[2],size(map)[1]*size(map)[2])
     dist_mat = fill(0, size(map)[1]*size(map)[2],size(map)[1]*size(map)[2]);
 
-    #node_counter = 1
     for i in 1:size(map)[1]
         for j in 1:size(map)[2]
             if((map[i,j]=="I")||(map[i,j]=="O")||(map[i,j]=="H")) 
-                println("TRUE ",i," ",j)
                 checkNeighbour(map, adj_mat, dist_mat, i, j)
-                #node_counter = node_counter + 1;
             end
         end
     end
@@ -112,7 +99,7 @@ function main()
     map = readMapCSV(filename,16,16)
     g , dist_mat = generateMapGraph(map)
 
-    @time path = a_star(g, 31, 146, dist_mat);
+    @time path = a_star(g, 31, 146, dist_mat, heuristic);
     println(path);
     println(dist_mat[16:32,16:32])
 
