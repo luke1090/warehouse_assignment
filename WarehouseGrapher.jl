7a8d1f8a5a03a56d668f7c1a478e8b59d1d7029d
Anthony Kremor <anthony@kremor.com.au>
2017-05-12 17:32:15 +1000

    Attempt at heuristic.

--- WarehouseGrapher.jl
+++ WarehouseGrapher.jl
@@ -4,11 +4,60 @@ using Colors
 using Compose
 
 
-costs = rand(1:1000, 16*16*16*16, 1)
-function heuristic(v)
-    return costs[v];
+global costs = Array(AbstractFloat, 1, 60 * 59 * 60 * 59);
+
+#=
+function computeCosts(target_node, x, y)
+    # Take a vertex and convert it into (x,y)
+    global costsMat = Array(AbstractFloat, 1, 60 * 59 * 60 * 59); # Global
+    # First divide through by the map size
+    for i in 1:x, j in 1:y
+        # Convert to node number
+        node_number = (i-1)*x + (j-1)*y + 1;
+        println("i-1 ", i-1, " ");
+        println("Node numb", node_number);
+
+        target_x = floor(target_node / x);
+        target_y = rem(target_node, y);
+        dx = abs(i - target_x)
+        dy = abs(j - target_y)
+        D = 1; # Straight line move
+        D2 = 1.41; # Diagonal move
+        costsMat[node_number] = D * (dx + dy) + (D2 - 2 * D) * min(dx, dy);
+    end
+
+    costs = deepcopy(costsMat);
+end
+=#
+
+target_node = 2698;
+x = 60;
+y = 59;
+global costs = Array(Int64, 1, 60 * 60 + 59); # Global
+# First divide through by the map size
+for i in 1:x, j in 1:y
+    # Convert to node number
+    node_number = (i-0)*x + (j-0);
+    #println("i-1 ", i-1, " ");
+    println("(i,j) = (", i, ",", j, ")    nn=", node_number);
+    println("Node numb", node_number);
+
+    target_x = floor(target_node / x) + 1;
+    target_y = rem(target_node, y);
+    dx = abs(i - target_x)
+    dy = abs(j - target_y)
+    D = 1; # Straight line move
+    D2 = 1.41; # Diagonal move
+    costs[node_number] = round(D * (dx + dy) + (D2 - 2 * D) * min(dx, dy));
+
 end
 
+
+
+function heuristic(v)
+    #println("Heurisitic returning ", convert(Int64, round(costs[v])));
+    return costs[v];
+end
 
 function nodeValue(char)
     if char == "O"
@@ -41,11 +90,11 @@ function checkNeighbour(map, adj_mat, dist_mat, i, j)
             y = j + y_offset;
             if ((x > 0) && (x <= size(map)[1]) && (y > 0) && (y <= size(map)[2]))
                 if isValidMove(map[x,y])
-                    println("\tTRUE ",x," ",y, " ", map[x,y])
+                    #println("\tTRUE ",x," ",y, " ", map[x,y])
                     adj_mat[(i-1)*size(map)[1] + j, (x-1)*size(map)[1] + y] = true
                     dist_mat[(i-1)*size(map)[1] + j, (x-1)*size(map)[1] + y] = nodeValue(map[x,y])
 
-                    println("\t\tTRUE ",(i-1)*size(map)[1] + j," ", (x-1)*size(map)[1] + (y));
+                    #println("\t\tTRUE ",(i-1)*size(map)[1] + j," ", (x-1)*size(map)[1] + (y));
                 end
             end
         end
@@ -92,25 +141,40 @@ function generateMapGraph(map)
     return g, dist_mat
 end
 
-#=
 function main()
-    filename = "/Users/Prakhar/Dropbox/Forklift/SmallWarehouseMap.csv"
+    filename = "WarehouseMap.csv"
+    x = 60;
+    y = 59;
 
-    map = readMapCSV(filename,16,16)
-    g , dist_mat = generateMapGraph(map)
+    start_node = 339;
+    end_node = 2698;
+    #computeCosts(end_node, x, y);
 
-    @time path = a_star(g, 31, 146, dist_mat, heuristic);
-    println(path);
-    println(dist_mat[16:32,16:32])
+println("Entering readmap");
+    map = readMapCSV(filename, x, y)
 
-    nodefillc = fill(colorant"lightseagreen", 256);
-
-    for node in path
-        #nodefillc[node[1]] = colorant"orange";
-    end
+    println("Entering mapgraph");
+    g, dist_mat = generateMapGraph(map)
 
     
+    println("Entering solver");
+    
 
-    draw(PDF("nodes.pdf", 160cm, 160cm), gplot(g, nodelabel=1:256, nodefillc=nodefillc))
+    @time path = a_star(g, start_node, end_node, dist_mat, heuristic);
+
+
+    #draw(PDF("nodes.pdf", 160cm, 160cm), gplot(g, nodelabel=1:x * y))
+    println(path);
+    #println(dist_mat[16:32,16:32])
+
+    nodefillc = fill(colorant"lightseagreen", x * y);
+
+    # Colour the nodes we travel through
+    for node in path
+        nodefillc[node[1]] = colorant"orange";
+    end
+
+    draw(PDF("nodes.pdf", 160cm, 160cm), gplot(g, nodelabel=1:x * y, nodefillc=nodefillc))
 end
-=#
+
+main();
