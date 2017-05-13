863721ac9d4870f1ab1e70e0e7508c00050f0546
luke1090 <luketornilla@gmail.com>
2017-05-13 12:27:38 +1000

    added function resolvecollisions(routearray)
    returns a routearray with pause nodes to resolve collisions
    handles multi-time location traversal

--- collisionchecker.jl
+++ collisionchecker.jl
@@ -69,3 +69,72 @@ function findcollisions(AR::Array)
     
     return collisionMatrix
 end
+
+#given job routes with conflicting timespaces
+#find vehicle with greater slack
+#-> add a "pause" node to that vehicle's route before the collision
+#-> assumes that all vehicles have unique starting positions
+#resolve collisions
+
+function resolvecollisions(AR::Array)
+    vehiclecount = size(AR,1)
+    
+    routelength = Array{Int}(vehiclecount)
+    # find the length of each vehicles route
+    for i in eachindex(AR)
+    #println(i)
+    routelength[i] = size(AR[i],1)
+    end
+
+    endtime = maximum(routelength)
+    
+    timeslider = 1
+    
+    while timeslider<=endtime
+        
+        #check if the cell exists, then compare to other rows if they exist
+        # if a collision is detected,
+        # find the vehicle with the shorter path
+        # add a pause node to that path BEFORE the collision location
+        for i in 1:vehiclecount
+            if timeslider<=routelength[i]
+                p1 = AR[i][timeslider]
+                for j in i+1:vehiclecount
+                    #println("vehicle",i," vs vehicle",j," at time",timeslider,"/",endtime)
+                    if timeslider<=routelength[j]
+                        p2 = AR[j][timeslider]
+                        if p1==p2
+                            # collision detected at node p1=p2
+                            # this decision needs to be changed for multi-timestep collisions
+                            # the pause node must be added to the vehicle that arrives LATER
+                            # should only ever have to look back 1 timestep
+                            if routelength[i]<=routelength[j]
+                                # add pause in vehicle i
+                                # default q value
+                                q = i
+                            else
+                                # add pause in vehicle j
+                                q = j
+                            end
+                            # then check if q value is consistent with arrival times
+                            # if unequal arrival time then use that q instead
+                            if AR[i][timeslider-1]==p1 && AR[j][timeslider-1]!=p1
+                                q = j
+                            elseif AR[i][timeslider-1]!=p1 && AR[j][timeslider-1]==p1
+                                q = i
+                            end
+                            
+                            println("adding pause to vehicle",q," at time",timeslider,"/",endtime)
+                            insert!(AR[q],timeslider,AR[q][timeslider-1])
+                            routelength[q] = size(AR[q],1)
+                            endtime = maximum(routelength)
+                        end
+                    end
+                end
+            end
+        end
+        timeslider = timeslider+1
+    end
+    
+    return AR
+end
